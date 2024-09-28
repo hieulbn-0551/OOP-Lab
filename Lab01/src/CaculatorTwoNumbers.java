55fe3733c64c10d8d0365b9a34496134ad6543e0
hieungoc16 <137400824+hieungoc16@users.noreply.github.com>
2024-09-28 11:39:48 +0700

    lab 01

---
+++ Lab01/src/CaculatorTwoNumbers.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+public class CaculatorTwoNumbers {
+    public static void main(String[] args) {
+        String strNum1 = JOptionPane.showInputDialog("Enter the first number:");
+        double num1 = Double.parseDouble(strNum1);
+
+        String strNum2 = JOptionPane.showInputDialog("Enter the second number:");
+        double num2 = Double.parseDouble(strNum2);
+
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+
+        String resultMessage = "Sum: " + sum + "\n" +
+                "Difference: " + difference + "\n" +
+                "Product: " + product + "\n";
+
+        if (num2 != 0) {
+            double quotient = num1 / num2;
+            resultMessage += "Quotient: " + quotient;
+        } else {
+            resultMessage += "Cannot divide by zero.";
+        }
+
+        JOptionPane.showMessageDialog(null, resultMessage);
+    }
+}
